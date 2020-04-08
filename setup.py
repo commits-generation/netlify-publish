507f15a09018166888619149a1205dd8ca54e492
Wassim Benzarti <wass11121996@gmail.com>
2020-04-08 09:56:33 +0000

    Renamed the package

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="commit-generation-datasets", # Replace with your own username
+    name="commit_generation_datasets", # Replace with your own username
     version="0.0.1",
     author="Wassim Benzarti",
     author_email="m.wassim.benzarti@gmail.com",
